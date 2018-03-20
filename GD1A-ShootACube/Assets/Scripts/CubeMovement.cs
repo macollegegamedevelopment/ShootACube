abc1d414eb6f726079ef89d5b4015bc8b44fe14e
Richard Ram <richard@richard-ram.com>
2018-03-20 14:58:31 +0100

    Updated projects after lesson 2

---
+++ GD1A-ShootACube/Assets/Scripts/CubeMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour 
+{
+    public float movementSpeed = 1;
+
+	private void Update()
+	{
+        transform.Translate(Vector3.down * movementSpeed * Time.deltaTime);
+	}
+}
---
+++ GD1B-ShootACube/Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour 
+{
+    public GameObject cubePrefab;
+    public float spawnRate = 1f;
+    public float spawnWidth = 2.3f;
+
+    private float timer = 0;
+
+	void Update () 
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= spawnRate)
+        {
+            SpawnCube();   
+        }
+        print(timer);
+	}
+
+    private void SpawnCube()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnWidth, spawnWidth),
+                                            transform.position.y, transform.position.z);
+        Instantiate(cubePrefab, spawnPosition, Quaternion.identity);
+
+        timer = 0;
+    }
+}
